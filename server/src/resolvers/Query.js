99c8a373db9b08c98135aef0195eadfe7a519c9a
Alison Reilly <reillyamr@gmail.com>
2021-02-09 14:42:50 -0500

    add function to find indiv user details

---
+++ server/src/resolvers/Query.js
@@ -0,0 +1,8 @@
+function findUser(parent, args, context) {
+    return context.prisma.user.findUnique({ where: { id: parent.id } })
+    //findUser: (_, { id }) => users.find((u) => u.id === id),
+}
+  
+  module.exports = {
+    findUser,
+  }
