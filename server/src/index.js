b262c124167648e8799a8c649281941989205ee6
Alison Reilly <reillyamr@gmail.com>
2021-02-09 13:40:58 -0500

    update index.js with primary server and reqs

--- server/src/index.js
+++ server/src/index.js
@@ -2,3 +2,32 @@ const { ApolloServer } = require('apollo-server');
 const { PrismaClient } = require('@prisma/client')
 const fs = require('fs');
 const path = require('path');
+
+
+
+const prisma = new PrismaClient()
+
+const server = new ApolloServer ({
+    typeDefs: fs.readFileSync(
+        path.join(__dirname, 'schema.graphql'),
+        'utf8'
+    ),
+    resolvers,
+    context: ({ req }) => {
+        return {
+            ...req,
+            prisma,
+            userId:
+            req && req.headers.authorization
+            ? getUserId(req)
+            : null
+        };
+        
+    }
+});
+
+server
+    .listen()
+    .then(({ url }) =>
+        console.log(`Server is running on ${url}`)
+    );
