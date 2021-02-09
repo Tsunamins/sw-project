3b864113a49a2fc54a67a06a52e18548676141d5
Alison Reilly <reillyamr@gmail.com>
2021-02-09 14:46:53 -0500

    update index with Query resolver

--- server/src/index.js
+++ server/src/index.js
@@ -2,8 +2,12 @@ const { ApolloServer } = require('apollo-server');
 const { PrismaClient } = require('@prisma/client')
 const fs = require('fs');
 const path = require('path');
+const Query = require('./resolvers/Query')
 
 
+const resolvers = {
+    Query,
+  }
 
 const prisma = new PrismaClient()
 
