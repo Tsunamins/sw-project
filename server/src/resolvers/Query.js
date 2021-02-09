972f410cadea21ce9f007dcc7b4faa5bd8c0fa83
Alison Reilly <reillyamr@gmail.com>
2021-02-09 15:43:51 -0500

    initial migrate

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -2,6 +2,8 @@ function findUser(parent, args, context) {
     return context.prisma.user.findUnique({ where: { id: parent.id } })
     //findUser: (_, { id }) => users.find((u) => u.id === id),
 }
+
+
   
   module.exports = {
     findUser,
