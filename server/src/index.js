73286acfad17623238c7b0c809cbad7ec6985693
Alison Reilly <reillyamr@gmail.com>
2021-02-10 11:54:30 -0500

    req mutation and add to resolvers

--- server/src/index.js
+++ server/src/index.js
@@ -2,24 +2,27 @@ const { ApolloServer } = require('apollo-server');
 const { PrismaClient } = require('@prisma/client')
 const fs = require('fs');
 const path = require('path');
-const Query = require('./resolvers/Query')
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
 
 
 const resolvers = {
     Query,
+    Mutation
   }
 
-const prisma = new PrismaClient()
 
+const prisma = new PrismaClient()
 const server = new ApolloServer({
     typeDefs: fs.readFileSync(
-      path.join(__dirname, 'schema.graphql'),
+        path.join(__dirname, 'schema.graphql'),
       'utf8'
     ),
     resolvers,
     context: {
       prisma,
     }
+    
   })
 
 server
