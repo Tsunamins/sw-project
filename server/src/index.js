86b5f97c9c1e312e7ede6bb0ec1da6d58df60224
Alison Reilly <reillyamr@gmail.com>
2021-02-10 16:03:15 -0500

    the rocket works so trying a cat

--- server/src/index.js
+++ server/src/index.js
@@ -28,5 +28,5 @@ const server = new ApolloServer({
 server
     .listen()
     .then(({ url }) =>
-        console.log(`🚀 Server is running on ${url}`)
+        console.log(`😺️ Server is running on ${url}`)
     );
