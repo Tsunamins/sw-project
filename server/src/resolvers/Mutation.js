d3eeaed994954131ccaed163b9807e195bd7853b
Alison Reilly <reillyamr@gmail.com>
2021-02-11 16:21:16 -0500

    add login and exports

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -29,6 +29,25 @@ async function signup(parent, args, context, info){
     }
 }
 
+async function login(parent, args, context, info){
+    const user = await context.prisma.user.findUnique({ where: { email: args.email } })
+    if(!user){
+        throw new Error('No such user found')
+    }
+
+    const valid = await bcrypt.compare(args.password, user.password)
+    if(!valid){
+        throw new Error('Invalid password')
+    }
+
+    const token = jwt.sign({ userId: user.id }, APP_SECRET)
+
+    return{
+        token,
+        user
+    }
+}
+
 // async function newUser(parent, args, context) {
 //     return await context.prisma.user.create({
 //         data: {
@@ -48,7 +67,7 @@ async function signup(parent, args, context, info){
 
   
   module.exports = {
-    singleUpload, 
+    singleUpload, signup, login,
   }
 
 
