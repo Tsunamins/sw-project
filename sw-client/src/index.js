1aec10379a00e2dff29ec89eeef3a2b0c5a9238c
Alison Reilly <reillyamr@gmail.com>
2021-02-11 17:46:31 -0500

    add apollo client configuration

--- sw-client/src/index.js
+++ sw-client/src/index.js
@@ -3,10 +3,27 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+});
+
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-  
+  <ApolloProvider client={client}>
     <App />
+  </ApolloProvider>
   ,
   document.getElementById('root')
 );
