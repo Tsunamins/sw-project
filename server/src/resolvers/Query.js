a20a5b902f111f9ff62ac25ae24a3c3dea1a6643
Alison Reilly <reillyamr@gmail.com>
2021-02-11 15:52:42 -0500

    install bcrypt and jwt

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -7,8 +7,18 @@ function allUsers(parent, args, context, info) {
     return context.prisma.user.findMany()
   }
 
+//query all file uploads for now
+//correct only if creating a database space for the file names/paths/ and user associations
+
+function uploads(parent, args, context){
+    return context.prisma.file.findMany()
+}
+
+
+
+
 
   
   module.exports = {
-    findUser, allUsers,
+    findUser, allUsers, 
   }
