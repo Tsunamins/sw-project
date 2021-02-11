cb37e43f17ea76762fbd68a94e412fab462644b0
Alison Reilly <reillyamr@gmail.com>
2021-02-11 16:20:18 -0500

    add signup and requirements

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -1,4 +1,7 @@
 const fs = require('fs');
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { APP_SECRET, getUserId } = require('../utils')
 
 function singleUpload(parent, args, context) {
     return args.file.then(file => {
@@ -13,26 +16,39 @@ function singleUpload(parent, args, context) {
       });
 }
 
-async function newUser(parent, args, context) {
-    return await context.prisma.user.create({
-        data: {
-            username: args.username,
-            name: args.name,
-            email: args.email,
-            password: args.password,
-            photo: args.photo
+async function signup(parent, args, context, info){
+    const password = await bcrypt.hash(args.password, 10)
+
+    const user = await context.prisma.user.create({ data: { ...args, password } })
+
+    const token = jwt.sign({ userId: user.id }, APP_SECRET)
+
+    return {
+        token,
+        user
+    }
+}
+
+// async function newUser(parent, args, context) {
+//     return await context.prisma.user.create({
+//         data: {
+//             username: args.username,
+//             name: args.name,
+//             email: args.email,
+//             password: args.password,
+//             photo: args.photo
             
-        }
-    })
+//         }
+//     })
  
-} 
+// }
 
 
 
 
   
   module.exports = {
-    singleUpload, newUser,
+    singleUpload, 
   }
 
 
