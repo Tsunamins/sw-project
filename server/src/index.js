37ad2b76455f8577f1c397c2330f9d59ae5799bb
Alison Reilly <reillyamr@gmail.com>
2021-02-11 16:37:43 -0500

    update server with auths, requre utils

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ const { ApolloServer } = require('apollo-server');
 const { PrismaClient } = require('@prisma/client')
 const fs = require('fs');
 const path = require('path');
+const { getUserId } = require('./utils');
 const Query = require('./resolvers/Query');
 const Mutation = require('./resolvers/Mutation')
 
@@ -13,17 +14,24 @@ const resolvers = {
 
 
 const prisma = new PrismaClient()
-const server = new ApolloServer({
+const server = new ApolloServer ({
     typeDefs: fs.readFileSync(
         path.join(__dirname, 'schema.graphql'),
-      'utf8'
+        'utf8'
     ),
     resolvers,
-    context: {
-      prisma,
+    context: ({ req }) => {
+        return {
+            ...req,
+            prisma,
+            userId:
+            req && req.headers.authorization
+            ? getUserId(req)
+            : null
+        };
+        
     }
-    
-  })
+});
 
 server
     .listen()
