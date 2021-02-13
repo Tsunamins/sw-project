387709748023397d9fe23e56a38e27812a81ec41
Alison Reilly <reillyamr@gmail.com>
2021-02-12 20:25:35 -0500

    add authReducer to store

--- sw-client/src/store/store.js
+++ sw-client/src/store/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
-import authReducer from './reducers/authReducer'
+import authReducer from './authReducer'
 
 const reducer = combineReducers({
     authReducer,
