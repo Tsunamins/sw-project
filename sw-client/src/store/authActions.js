87bdacb9b0f3aaa0118f7399d74cbb0e5cf5fb19
Alison Reilly <reillyamr@gmail.com>
2021-02-12 20:25:09 -0500

    create a login action, action creator for user

---
+++ sw-client/src/store/authActions.js
@@ -0,0 +1,26 @@
+import { AUTH_TOKEN } from '../constants';
+
+
+export const login = (user) => {
+    return dispatch => {
+        localStorage.setItem(AUTH_TOKEN, login.token);
+        dispatch(sessionUser)
+    }
+}
+
+
+
+
+
+
+
+
+export const sessionUser = user => {
+    return {
+        type: "CURRENT_USER"
+    }
+}
+
+
+
+
