b6b659bcc963ca7a8ba2f7a7ff5305e362df7d96
Alison Reilly <reillyamr@gmail.com>
2021-02-12 20:24:38 -0500

    add rename of LoginForm

--- sw-client/src/App.js
+++ sw-client/src/App.js
@@ -1,10 +1,11 @@
 import './styles/App.css';
 import { Redirect, Route, Switch } from 'react-router-dom';
+import LoginForm from './components/LoginForm'
 
 function App() {
   return (
     <div className="App">
-  
+      <LoginForm />
     </div>
   );
 }
