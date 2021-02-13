ac18e64cef6519aa76ec556739cd21ee4f757f68
Alison Reilly <reillyamr@gmail.com>
2021-02-12 20:28:12 -0500

    add reducer, concept as session user

---
+++ sw-client/src/store/authReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+    token: null,
+    current: null
+}
+
+export default function auth(state=initialState, action) {
+    switch (action.type) {
+   
+        case 'SESSION_USER':
+            return {...state,
+                current: action.user,
+            };  
+    }
+}
