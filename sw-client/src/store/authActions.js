ccf75cbd05ffc0c12f5dc2d641d022e993e4197f
Alison Reilly <reillyamr@gmail.com>
2021-02-13 16:16:38 -0500

    add mut to fetch, update naming, connect w reducer

--- sw-client/src/store/authActions.js
+++ sw-client/src/store/authActions.js
@@ -1,26 +1,50 @@
 import { AUTH_TOKEN } from '../constants';
 
 
-export const login = (user) => {
+
+export const login = (data) => {
     return dispatch => {
-        localStorage.setItem(AUTH_TOKEN, login.token);
-        dispatch(sessionUser)
+
+        const graphql = JSON.stringify({
+            query: `mutation {
+                login(email: "graycie@sobe.com", password: "password") {
+                token
+                user {
+                    email
+                    username
+                    name
+                
+                    }
+                }
+                }`,
+                variables: {}
+        })
+        const requestOptions = {
+            method: 'POST',
+            headers: { "Content-Type": "application/json", "Accept": "application/json"},
+            body: graphql,
+            redirect: 'follow'
+        };
+
+        fetch("http://localhost:4000/", requestOptions)
+        .then(resp => resp.json())
+        .then(response => {
+            if(response.error){
+                alert(response.error)
+            } else {
+                console.log(response)
+                localStorage.setItem(AUTH_TOKEN, user.token);
+                dispatch(session(response))
+            }
+        }) 
+        .catch(error => console.log('error', error));
     }
-}
+} 
 
 
-
-
-
-
-
-
-export const sessionUser = user => {
+export const session = user => {
+    console.log(user)
     return {
         type: "SESSION_USER"
     }
 }
-
-
-
-
