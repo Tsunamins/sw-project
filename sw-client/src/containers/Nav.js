a505054b2bc12f81b3a96d0a082b1c3ee53e3386
Alison Reilly <reillyamr@gmail.com>
2021-02-14 12:48:30 -0500

    add links to Nav

--- sw-client/src/containers/Nav.js
+++ sw-client/src/containers/Nav.js
@@ -1,18 +1,22 @@
 import React from 'react'
-import { Link, Route } from 'react-router-dom'
-import LoginForm from '../components//LoginForm'
-import SignupForm from '../components//SignupForm'
+import { Link } from 'react-router-dom'
+
 
 class Nav extends React.Component {
 
     render(){
       return (
           <div id="Nav" >
+                <div>
+                    <Link to="/">Welcome</Link> 
+                    <Link to="/profile">Profile</Link> 
+
+                </div>
+                
                 <div id="AuthLinks">
                     <Link to="/signup">Sign Up</Link> 
                     <Link to="/login">Log In</Link>
-                    <Route exact path='/signup' component={SignupForm}/>
-                    <Route exact path='/login' component={LoginForm}/>
+                   
                 </div> 
         </div>
       );
