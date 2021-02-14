2706bbbf58d273abc290ec90d686cf7653c7adc8
Alison Reilly <reillyamr@gmail.com>
2021-02-14 18:47:26 -0500

    add upload function

--- sw-client/src/store/authActions.js
+++ sw-client/src/store/authActions.js
@@ -36,16 +36,38 @@ export const auth = (mut) => {
     }
 }
 
+export const upload = (file, photo) => {
+    console.log(file)
+    console.log(photo)
+
+    let formdata = new FormData();
+    formdata.append("operations", "{ \"query\": \"mutation ($file: Upload!) { singleUpload(file: $file) {filename} }\", \"variables\": { \"file\": null } }");
+    formdata.append("map", "{ \"0\": [\"variables.file\"] }");
+    formdata.append("0", file, `${photo}`);
+  
+    let requestOptions = {
+        method: 'POST',
+        body: formdata,
+        redirect: 'follow'
+    };
+
+    fetch("http://localhost:4000/", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
 
 
 
-export const session = user => {
-    console.log(user)
-    return {
-        type: "SESSION_USER",
-        user
-    }
 }
+
+    export const session = user => {
+        console.log(user)
+        return {
+            type: "SESSION_USER",
+            user
+        }
+    }
+
 
 
 
