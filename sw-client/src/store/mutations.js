ae273296c3b21fa5147ee93c4eb7621925135feb
Alison Reilly <reillyamr@gmail.com>
2021-02-14 14:53:13 -0500

    change mutation to whole JSON stringify need

--- sw-client/src/store/mutations.js
+++ sw-client/src/store/mutations.js
@@ -1,75 +1,40 @@
-import { gql } from '@apollo/client';
-import { login } from './authActions'
-
-
-
-export const LOGIN_MUTATION = gql`
-  mutation LoginMutation(
-    $email: String!
-    $password: String!
-  ) {
-    login(email: $email, password: $password) {
-      token,
-      user{
-        email,
-        username,
-        name,
-        photo
-      }
-    }
-  }
-`;
-
-const SIGNUP_MUTATION = gql`
-  mutation SignupMutation(
-    $name: String!
-    $username: String!
-    $email: String!
-    $photo: String
-    $password: String!
-    
-  ) {
-    signup(
-      name: $name
-      username: $username
-      email: $email
-      photo: $photo
-      password: $password
-      
-    ) {
-      token
-    }
-  }
-`;
-
 export const loginMutation = (data) => {
-  const query = `mutation {
-        login(email: "${data.email}", password: "${data.password}") {
-          token
-          user {
-            email
-            username
-            name
-            photo
-           
-            }
-          }
-        }`
-        return query
+        const mut = JSON.stringify({
+          query: `mutation {
+            login(email: "${data.email}", password: "${data.password}") {
+              token
+              user {
+                email
+                username
+                name
+                photo
+               
+                }
+              }
+            }`,
+          variables: {}
+        })
+        return mut
 }
 
 export const signupMutation = (data) => {
-  const query = `mutation {
-        signup(name: "${data.name}", username: "${data.username}", email: "${data.email}", photo: "${data.photo}:, password: "${data.password}") {
-          token
-          user {
-            email
-            username
-            name
-            photo
-           
-            }
-          }
-        }`
-        return query
+  const mut = JSON.stringify({
+    query: `mutation {
+      signup(username: "${data.username}", name: "${data.name}", email: "${data.email}", photo: "${data.photo}", password: "${data.password}") {
+        token
+        user {
+          id
+          name
+          username
+          email
+          photo
+          
+        }
+      }
+    }`,
+    variables: {}
+  })
+
+
+  return mut
 }
