fe5a19427c7294c0082670e6dd38e9d98cf72783
Alison Reilly <reillyamr@gmail.com>
2021-02-13 19:39:33 -0500

    update login and session w resp and user

--- sw-client/src/store/authActions.js
+++ sw-client/src/store/authActions.js
@@ -16,54 +16,27 @@ export const login = (data) => {
 
         
         .then(resp => resp.json())
-        .then(response => console.log(response.data.login.user))
-        .then(response => dispatch(session(response.data.login.user)))
-        .catch(error => console.log('error', error));
+        .then(response => {
+            if(response.error){
+                alert(response.error)
+            } else {
+                console.log(response.data.login.user)
+                //local storage
+                //maybe separate dispatch for token
+                dispatch(session(response.data.login.user))
+            }
+        })
+        .catch(console.log);
 
     }
 }
 
 
-
-
-
-
-
-
-// export const login = () => {
-//     return dispatch => {
-
-//         var graphql = JSON.stringify({
-//             query: "mutation {\n  login(email: \"graycie@sobe.com\", password: \"password\") {\n    token\n    user {\n      email\n      username\n      name\n     \n      }\n    }\n  }",
-//             variables: {}
-//           })
-
-//         var requestOptions = {
-//             method: 'POST',
-//             headers: { "Content-Type": "application/json", "Accept": "application/json"},
-//             body: graphql,
-//             redirect: 'follow'
-//         };
-
-//         fetch("http://localhost:4000/", requestOptions)
-//         .then(resp => resp.json())
-//         .then(response => {
-//             if(response.error){
-//                 alert(response.error)
-//             } else {
-//                 console.log(response)
-//                 localStorage.setItem(AUTH_TOKEN, response.token);
-//                 dispatch(session(response))
-//             }
-//         }) 
-//         .catch(error => console.log('error', error));
-//     }
-// }
-
 export const session = user => {
     console.log(user)
     return {
-        type: "SESSION_USER"
+        type: "SESSION_USER",
+        user
     }
 }
 
