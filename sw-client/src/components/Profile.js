824ae088eddfa97b925d73840ff5c8dfd65e4772
Alison Reilly <reillyamr@gmail.com>
2021-02-14 19:47:24 -0500

    add data to Profile when logged in

--- sw-client/src/components/Profile.js
+++ sw-client/src/components/Profile.js
@@ -1,13 +1,33 @@
 import React from 'react';
-
+import { connect } from 'react-redux'
 
 const Profile = (props) => {
+    const session = props.session
+
   return (
+    session ? 
+   
     <div> 
         <h1>Profile</h1>
+
+        <h3>Name: {props.session.name}</h3>
+        <h3>Username: {props.session.username}</h3>
+        <h3>Email: {props.session.email}</h3>
     </div>
+    :
+    <div></div> 
   );
+    
 };
 
+const mapStateToProps = state => {
+    console.log(state)
+    return {
+   
+      session: state.authReducer.current
+  
+    }
+  }
 
-export default Profile
+
+export default connect(mapStateToProps)(Profile);
