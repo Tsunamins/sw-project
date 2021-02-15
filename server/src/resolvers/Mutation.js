71d7d8fd66b8d0d43eb67392dcc9f4fb60f59b9c
Alison Reilly <reillyamr@gmail.com>
2021-02-15 12:49:55 -0500

    remove example

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -16,6 +16,8 @@ function singleUpload(parent, args, context) {
       });
 }
 
+
+
 async function signup(parent, args, context, info){
     console.log("attempting signup")
     const password = await bcrypt.hash(args.password, 10)
@@ -55,23 +57,6 @@ async function login(parent, args, context, info){
         user
     }
 }
-
-// async function newUser(parent, args, context) {
-//     return await context.prisma.user.create({
-//         data: {
-//             username: args.username,
-//             name: args.name,
-//             email: args.email,
-//             password: args.password,
-//             photo: args.photo
-            
-//         }
-//     })
- 
-// }
-
-
-
 
   
   module.exports = {
