0471755ad01d300e5650ddb5e2cfefa588c059c5
Alison Reilly <reillyamr@gmail.com>
2021-02-14 19:48:01 -0500

    map state to props for session

--- sw-client/src/containers/Nav.js
+++ sw-client/src/containers/Nav.js
@@ -1,26 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 
 class Nav extends React.Component {
-
+  
     render(){
+      console.log(this.props)
       return (
           <div id="Nav" >
                 <div>
-                    <Link to="/">Welcome</Link> 
+                    <Link to="/">Welcome</Link> |
                     <Link to="/profile">Profile</Link> 
 
                 </div>
                 
                 <div id="AuthLinks">
-                    <Link to="/signup">Sign Up</Link> 
+                    <Link to="/signup">Sign Up</Link> |
                     <Link to="/login">Log In</Link>
-                   
                 </div> 
+
         </div>
       );
     }
   }
  
-export default Nav;
+  const mapStateToProps = state => {
+    console.log(state)
+    return {
+   
+      session: state.authReducer.current
+  
+    }
+  }
+
+
+export default connect(mapStateToProps)(Nav);
